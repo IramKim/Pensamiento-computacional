52e0f7dc698e7cdd35bc7431c6c07beffa9b6ed0
IramKim <111139847+IramKim@users.noreply.github.com>
2022-10-13 11:19:19 -0500

    Add files via upload

--- CalculadoraRM.py
+++ CalculadoraRM.py
@@ -1,18 +1,33 @@
-#formula de Gorostiaga 1RM
+#formula de Gorostiaga para 1RM
+    """
+    recibe el peso y las repeticiones trabajadas y
+    utilizando una formula particulas, calcula el 1RM.
+    Y retorna el 1RM
+    """
 
 def calc_1rm(ing_peso,ing_rep):
     rm_1 = ing_peso / (1.0278-(0.0278*ing_rep))
     return rm_1
 
-#coeficientes y formulas
+#coeficientes y formulas para calcúlos
 
 def valid():
+     """
+    no recibe ningun parametro, solo da el paso para
+    validar si el programa puede calcular lo deseado
+    """
+     
     while ing_rm <= 12:
         return (calc_rm(ing_rm))
     else:
-        return ("Introduce un numero valido")
+        return False
     
 def coeficiente(ing_rm):
+    """
+    recibe el ing_rm deseado y calcula el coeficiente por
+    el cual se multiplicara el 1rm, 100%(1RM) = 1. Y retorna
+    el coeficiente(acum)
+    """
     cont = 1
     acum = 1.05
     while cont <= ing_rm:
@@ -21,12 +36,22 @@ def coeficiente(ing_rm):
     return acum  
       
 def calc_rm(ing_rm):
+    """
+    recibe el ing_rm deseado y utilizando el coeficiente se
+    multiplica por el 1RM internamente calculado. Y retorna
+    el el valor del ing_rm deseado (rm)
+    """
     for i in range(0,13):
         if i == ing_rm:
             rm = ((calc_1rm(ing_peso,ing_rep)) * (coeficiente(ing_rm)))
             return("tu", ing_rm, "RM es: %.1f"%rm)
 
 def calc_rms_todos():
+    """
+    no recibe ningun parametro ya que solo calculara de
+    manera adicional otros RMs por si el usuario desea conocerlos.
+    Y retorna la lista con los valores(lista_res)
+    """
     i = 1
     cont = 0
     acum = 1.05
@@ -41,6 +66,11 @@ def calc_rms_todos():
 
 
 def lista_rms(ing_rep,ing_peso):
+    """
+    recibe el ing_rep y el ing_peso para desplegar la
+    lista de los RM en conjunto con la función anterior. Y
+    retorna una lista con un mensaje
+    """
     lista = [ing_rep,ing_peso]
     lista_rm = (list(range(1,13)))
     lista_res = calc_rms_todos()
@@ -55,6 +85,10 @@ def lista_rms(ing_rep,ing_peso):
     print("")
     
 def tabla_de_equivalencias():
+    """
+    Despliega una tabla de equivalencias de Kg a Lb en forma de listas
+    anidadas por si el usuario lo necesita conocer. Y retorna la matriz
+    """
     matrix = []
     lista= [1,5,10,20,30]
     i = 0
@@ -68,41 +102,43 @@ def tabla_de_equivalencias():
         i = i + 1
         matrix.append(columna)
     print(matrix)
-                
-
+                             
 #datos pedidos
 
 ing_peso = float(input("Ingrese el peso trabajado en kg: "))
 
 ing_rep = int(input("A cuantas repeticiones?: "))
 
-ing_rm = int(input("Que RM quisieras saber? 1-12: "))
+ing_rm = int(input("Que RM quisierás saber? 1-12: "))
 
 #mostrar resultado
 
 print("")
 
-pausa = input("presiona ENTER para mostrar resultados")
+if valid() == False:
+    print ("Introduce un número válido")
+else:
+    pausa = input("presiona ENTER para mostrar resultados")
 
-print("")
+    print("")
 
-print(valid())
+    print(valid())
 
-print("")
+    print("")
 
-pausa = input("Quieres conocer otros resultados? presiona ENTER ")
+    pausa = input("Quieres conocer otros resultados? presiona ENTER ")
 
-print("")
+    print("")
 
-(lista_rms(ing_rep,ing_peso))
+    (lista_rms(ing_rep,ing_peso))
 
-print("""NOTA: La calculadora no puede calcular los RM reales que sean mayores al número de repeticiones ingresado, debido
-a que ese número de repeticiones esta siendo considerado usando el 100% de la capacidad de tu cuerpo para generar fuerza,
-este programa es idoneo para conocer RMs menores que 5, no mayores.
+    print("""NOTA: La calculadora no puede calcular los RM reales que sean mayores al número de repeticiones ingresado, debido
+    a que ese número de repeticiones esta siendo considerado usando el 100% de la capacidad de tu cuerpo para generar fuerza,
+    este programa es idoneo para conocer RMs menores que 5, no mayores.
 
-Ej = no es coherente que el 12 RM de 60kg a 12 repeticiones sea menor que 60kg.""","\n")
+    Ej = no es coherente que el 12 RM de 60kg a 12 repeticiones sea menor que 60kg.""","\n")
 
-tabla_de_equivalencias()
+    tabla_de_equivalencias()
 
 
 
